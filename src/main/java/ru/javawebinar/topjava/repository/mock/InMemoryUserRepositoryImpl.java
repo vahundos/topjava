2996a0925670f2130dcda644a5b0cac6faa5485e
Vladimir Avdeev <vahundos@gmail.com>
2017-11-11 19:53:13 +0200

    in memory user repository implementation

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,60 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return repository.remove(id) != null;
+    }
+
+    @Override
+    public User save(User user) {
+        log.info("save {}", user);
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+        }
+        repository.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("getAll");
+        List<User> users = new ArrayList<>(repository.values());
+        users.sort(Comparator.comparing(User::getName));
+        return users;
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+        for (User user : repository.values()) {
+            if (email.equals(user.getEmail())) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
